5eacd8aa0a8d9dc996e2592255393456359af99e
习习风 <fastquery@126.com>
2016-07-27 11:10:50 +0800

    新增测试文件

---
+++ src/test/java/org/fastquery/bean/UserInformation.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2016-2016, fastquery.org and/or its affiliates. All rights reserved.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * For more information, please see http://www.fastquery.org/.
+ * 
+ */
+
+package org.fastquery.bean;
+
+/**
+ *
+ * @author xixifeng (fastquery@126.com)
+ */
+public class UserInformation {
+
+	private Integer uid;
+	private String myname;
+	private Integer myage;
+	
+	public Integer getUid() {
+		return uid;
+	}
+	public String getMyname() {
+		return myname;
+	}
+	public Integer getMyage() {
+		return myage;
+	}
+	public void setUid(Integer uid) {
+		this.uid = uid;
+	}
+	public void setMyname(String myname) {
+		this.myname = myname;
+	}
+	public void setMyage(Integer myage) {
+		this.myage = myage;
+	}
+	@Override
+	public String toString() {
+		return "UserInformation [uid=" + uid + ", myname=" + myname + ", myage=" + myage + "]";
+	}
+	
+	
+}
